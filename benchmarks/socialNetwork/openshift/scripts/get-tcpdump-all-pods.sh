0048e503a18ed9a2ebd50ad4a11206468e302c93
acemaster <vivekhtc25@gmail.com>
2022-08-22 05:32:11 -0500

    Added changes to enable restarts

--- benchmarks/socialNetwork/openshift/scripts/get-tcpdump-all-pods.sh
+++ benchmarks/socialNetwork/openshift/scripts/get-tcpdump-all-pods.sh
@@ -1,33 +1,70 @@
 #!/bin/bash
-
+read -p "Script mode: \n 1. TCP Dump \n 2.Jaeger " input_choice
+read -p "Start from scratch? Y/N" scratch_choice
+echo "Please make sure you have logged in to your kubernetes cluster....."
 rm -rf tcpData
 mkdir tcpData
+#Deploy service if doing from scratch
+if [[ $scratch_choice == [yY] ]]
+then
+#  read -p "Docker Username: " docker_username
+#  read -p "Docker Password: " docker_password
+#  read -p "Docker Email Id: " docker_email
+#  oc create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username="$docker_username" --docker-password="$docker_password" --docker-email="$docker_email"
+#  oc get secret regcred --output=yaml
+#  sh deploy-all-services-and-configurations.sh
+#  total_pods=$(oc get pods  -n social-network --output name | wc -l)
+#  while [ $(oc get pods  -n social-network --output name --field-selector=status.phase=Running | wc -l) -ne "$total_pods" ]; do
+#     sleep 60
+#  done
+  ubuntuclient=$(oc -n social-network get pod | grep ubuntu-client- | cut -f 1 -d " ")
+  oc cp "/Users/acemaster/Documents/BU Code/RA/DeathStarBench" social-network/"${ubuntuclient}":/root
+  oc exec "$ubuntuclient" -- bash -c "cd /root/DeathStarBench/socialNetwork/wrk2 && make clean && make"
+  sleep 60
+fi
 ALL_PODS=$(oc get pod -n social-network --field-selector=status.phase=Running -o custom-columns=name:metadata.name --no-headers)
-echo "Starting tcpdump for all pods ....."
-pid_arr=()
-for pod in $ALL_PODS; do
-  echo "Starting ksniff on pod: $pod"
-  oc sniff -p "$pod" -n social-network -o tcpData/output-"$pod".pcap & pid=$!
-  pid_arr+=("$pid")
-done
-sleep 60
-echo "Wait for all ksniff pods to be up...."
-total_pods=$(oc get pods  -n social-network --output name | wc -l)
-while [ $(oc get pods  -n social-network --output name --field-selector=status.phase=Running | wc -l) -ne "$total_pods" ]; do
-   sleep 5
-done
-echo "Running workload...."
-ubuntuclient=$(oc -n social-network get pod | grep ubuntu-client- | cut -f 1 -d " ")
-oc exec "$ubuntuclient" -- bash -c "cd /root/DeathStarBench/socialNetwork/wrk2 && \
-      ./wrk -D exp -t 2 -c 10 -d 30s -L -s ./scripts/social-network/read-user-timeline.lua http://nginx-thrift.social-network.svc.cluster.local:8080/wrk2-api/user-timeline/read -R 20"
-sleep 1m
-echo "Killing all knsiff processes...."
-for value in "${pid_arr[@]}"
-do
-     kill -9 "$value"
-done
-echo "Deleting all ksniff pods...."
-oc delete pods -l app=ksniff
-sleep 30
-mergecap -w tcpData/final-output.pcap tcpData/*.pcap
-python read-pcap-file.py
+# Starting script in TCP Dump mode
+if [[ $input_choice -eq 1 ]]
+then
+  sh configmaps/update-jaeger-configmap-disabled.sh
+  sh restartall.sh
+  echo "Starting tcpdump for all pods ....."
+  pid_arr=()
+  for pod in $ALL_PODS; do
+    echo "Starting ksniff on pod: $pod"
+    oc sniff -p "$pod" -n social-network -o tcpData/output-"$pod".pcap & pid=$!
+    pid_arr+=("$pid")
+  done
+  sleep 60
+  echo "Wait for all ksniff pods to be up...."
+  total_pods=$(oc get pods  -n social-network --output name | wc -l)
+  while [ $(oc get pods  -n social-network --output name --field-selector=status.phase=Running | wc -l) -ne "$total_pods" ]; do
+     sleep 60
+  done
+  echo "Running workload...."
+  ubuntuclient=$(oc -n social-network get pod | grep ubuntu-client- | cut -f 1 -d " ")
+  oc exec "$ubuntuclient" -- bash -c "cd /root/DeathStarBench/socialNetwork/wrk2 && \
+        ./wrk -D exp -t 2 -c 10 -d 30s -L -s ./scripts/social-network/read-user-timeline.lua http://nginx-thrift.social-network.svc.cluster.local:8080/wrk2-api/user-timeline/read -R 20"
+  sleep 1m
+  echo "Killing all knsiff processes...."
+  for value in "${pid_arr[@]}"
+  do
+       kill -9 "$value"
+  done
+  echo "Deleting all ksniff pods...."
+  oc delete pods -l app=ksniff
+  sleep 30
+  mergecap -w tcpData/final-output.pcap tcpData/*.pcap
+  python read-pcap-file.py
+fi
+# Starting script in Jaeger mode
+if [[ $input_choice -eq 2 ]]
+then
+  sh configmaps/update-jaeger-configmap.sh
+  sh restartall.sh
+  echo "Running workload...."
+  ubuntuclient=$(oc -n social-network get pod | grep ubuntu-client- | cut -f 1 -d " ")
+  oc exec "$ubuntuclient" -- bash -c "cd /root/DeathStarBench/socialNetwork/wrk2 && \
+        ./wrk -D exp -t 2 -c 10 -d 30s -L -s ./scripts/social-network/read-user-timeline.lua http://nginx-thrift.social-network.svc.cluster.local:8080/wrk2-api/user-timeline/read -R 20"
+  sleep 1m
+fi
