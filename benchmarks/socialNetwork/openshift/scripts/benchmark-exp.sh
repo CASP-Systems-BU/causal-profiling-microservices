e2103662e598f856ad047e37278359cc683edf82
acemaster <vivekhtc25@gmail.com>
2022-08-31 01:23:01 -0400

    Added changes with hdr plot and images

--- benchmarks/socialNetwork/openshift/scripts/benchmark-exp.sh
+++ benchmarks/socialNetwork/openshift/scripts/benchmark-exp.sh
@@ -6,4 +6,5 @@ do
   ./get-tcpdump-all-pods.sh "${args[0]}" "${args[1]}" "${args[2]}" "${args[3]}" <<< 1
   ./get-tcpdump-all-pods.sh "${args[0]}" "${args[1]}" "${args[2]}" "${args[3]}" <<< 3
   ./get-tcpdump-all-pods.sh "${args[0]}" "${args[1]}" "${args[2]}" "${args[3]}" <<< 2
+  hdr-plot --output benchmark-plot/benchmark-"${args[3]}".png --title "Benchmark for ${args[3]} rps" benchmark-exp-logs/experiment-1-${args[3]}.log benchmark-exp-logs/experiment-2-${args[3]}.log benchmark-exp-logs/experiment-3-${args[3]}.log
 done < "$input"
